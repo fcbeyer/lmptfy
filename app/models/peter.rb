0fb021b021f84c4cc712f9be29f67c6e30c8ce46
Rick Beyer <rick.beyer@gmail.com>
2013-11-07 21:46:25 -0500

    updated peter model to have two self defined methods that return all the peter emails or a random one.

--- app/models/peter.rb
+++ app/models/peter.rb
@@ -3,6 +3,13 @@ class Peter < ActiveRecord::Base
   
   def self.all_the_peters
     #get all the peters email addresses and return as a list for the "to" field on the email
-    return Peter.select(:email).where(:active => true).all 
+    list = []
+    Peter.select(:email).where(:active => true).all.each {|x| list.push(x.email)}
+    return list.join(",")
+  end
+  
+  def self.random_peter
+    num = Random.new.rand(1..Peter.last.id)
+    return Peter.find(num).email
   end
 end
